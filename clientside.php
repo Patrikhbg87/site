49fe3aac7d3afed44dd1de34858f182017242967
Patrik Malmgren Rask <patrik.malmgren.rask@comhem.se>
2017-03-29 12:41:11 +0200

    Delete clientside.php

--- clientside.php
+++
@@ -1,8 +0,0 @@
-<ul>
-	<li>HTML <span class="procent">90%</span></li>
-	<li>HTML5 <span class="procent">90%</span></li>
-	<li>CSS <span class="procent">90%</span></li>
-	<li>CSS3 <span class="procent">85%</span></li>
-	<li>Jquery <span class="procent">10%</span></li>
-	<li>Responsive design <span class="procent">90%</span></li>
-</ul>
