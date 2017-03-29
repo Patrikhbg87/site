e05d9ec84bd93122f3fa19c8c45b1dc0340cd21f
Patrik Malmgren Rask <patrik.malmgren.rask@comhem.se>
2017-03-29 12:32:52 +0200

    Delete serverside.php

--- serverside.php
+++
@@ -1,6 +0,0 @@
-<ul>
-	<li>PHP <span class="procent">20%</span></li>
-	<li>MySQL <span class="procent">10%</span></li>
-	<li>Apache <span class="procent">50%</span></li>
-	<li>Host <a class="procent" href="http://www.reprisehosting.com/client/aff.php?aff=203" target="_blank">Reprisehosting</a></li>
-</ul>
